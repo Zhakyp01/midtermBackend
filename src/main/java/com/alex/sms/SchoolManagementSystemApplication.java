db7297dc619f0966cf1f032aa092497ab06a5de1
Zhakyp Zhoomart uulu <zhakyp.zhoomartuulu.kk@gamil.com>
2022-04-05 17:57:02 +0600

    fronted

---
+++ src/main/java/com/alex/sms/SchoolManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.alex.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemApplication {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemApplication.class, args);
+		System.out.println("Running...");
+	}
+
+}
